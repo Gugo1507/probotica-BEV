f9d0f9a3ed5205a9e8e79e3a70bfbd07f981edc3
Bruno Gugo <152968309+Gugo1507@users.noreply.github.com>
2026-03-19 13:08:17 +0000

    Delete SEEKER_bev directory

--- SEEKER_bev/reset.py
+++
@@ -1,6 +0,0 @@
-import pyrealsense2 as rs
-
-ctx = rs.context()
-for dev in ctx.query_devices():
-    print("Resetting device:", dev.get_info(rs.camera_info.serial_number))
-    dev.hardware_reset()
